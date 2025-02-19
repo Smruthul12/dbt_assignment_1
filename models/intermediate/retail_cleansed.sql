1d96214bfb85e31c648b8c1972a8267bcd3649f7
Smruthul12 <msmruthul2002@gmail.com>
2025-02-19 16:12:39 +0530

    dbt tests completed

--- models/intermediate/retail_cleansed.sql
+++ models/intermediate/retail_cleansed.sql
@@ -11,18 +11,22 @@ WITH filtered_data AS (
         TRIM(UPPER(Description)) AS Description,
         Quantity,
         CAST(InvoiceDate AS DATE) AS InvoiceDate,  
-        CAST(UnitPrice_str AS FLOAT) AS UnitPrice,
+        CASE 
+            WHEN UnitPrice_str REGEXP '^[0-9]+(\.[0-9]+)?$' 
+                 AND CAST(UnitPrice_str AS FLOAT) > 0 
+            THEN CAST(UnitPrice_str AS FLOAT)
+            ELSE NULL  -- Set invalid/negative values to NULL
+        END AS UnitPrice,
         CustomerID,
         TRIM(UPPER(Country)) AS Country,
         ROW_NUMBER() OVER (PARTITION BY InvoiceNo ORDER BY InvoiceDate DESC) AS row_num
     FROM {{ ref('raw_retail') }}
     WHERE 
-         InvoiceNo IS NOT NULL 
-          AND CustomerID IS NOT NULL 
-          AND Description IS NOT NULL 
-          AND Description REGEXP '^[A-Za-z0-9 ]+$'
-          AND Quantity > 0
-          AND UnitPrice > 0 
+        InvoiceNo IS NOT NULL 
+        AND CustomerID IS NOT NULL 
+        AND Description IS NOT NULL 
+        AND Description REGEXP '^[A-Za-z0-9 ]+$'
+        AND Quantity > 0
 )
 
 SELECT 
@@ -35,5 +39,6 @@ SELECT
     CustomerID,
     Country
 FROM filtered_data
-WHERE row_num = 1  
+WHERE row_num = 1 
+AND UnitPrice IS NOT NULL
 ORDER BY InvoiceDate DESC
